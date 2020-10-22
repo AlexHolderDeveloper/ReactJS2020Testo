b308b9d936fcaf8a5cfb02766079faff3281051c
AlexHolderDeveloper <alex.holder@bigfootds.com>
2020-10-22 23:09:00 +0000

    Update gh-pages to output generated at 8140f7f

---
+++ precache-manifest.75dabbe3068ed70ff65eba9acf47efc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7e0768171d276acdbe0b9ef0abc8b2",
+    "url": "/reactjs2020testo/index.html"
+  },
+  {
+    "revision": "b2a7903f2f22f6dac042",
+    "url": "/reactjs2020testo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2848bdef96134e7a0844",
+    "url": "/reactjs2020testo/static/js/2.f09f6a62.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/reactjs2020testo/static/js/2.f09f6a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a7903f2f22f6dac042",
+    "url": "/reactjs2020testo/static/js/main.70aae26b.chunk.js"
+  },
+  {
+    "revision": "bb7cbfd2fe1707e575f1",
+    "url": "/reactjs2020testo/static/js/runtime-main.661f2f78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs2020testo/static/media/logo.5d5d9eef.svg"
+  }
+]);
